ff24f18d3ebd36b885b94191f580856044d982e3
ganesh <ganeshsadhashivam@outlook.com>
2022-12-15 07:20:31 +0530

    changes related to Error

--- models/products.modal.js
+++ models/products.modal.js
@@ -15,6 +15,13 @@ const productsSchema = new Schema(
   }
 );
 
+productsSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 const ProductsSchema = mongoose.model("ProductsSchema ", productsSchema);
 
 module.exports = ProductsSchema;
--- routes/products.js
+++ routes/products.js
@@ -1,30 +1,11 @@
-const router = require("express").Router();
+const { Router } = require("express");
+
 const Products = require("../models/products.modal");
 
-const multer = require("multer");
-//const upload = multer({ dest: "./uploads/" });
+const productsRouter = Router();
 
-//const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/pmg")
-    cb(null, false);
-  cb(null, true);
-};
-
-const upload = multer({ storage: storage });
-
-//GET Request
-router.route("/").get((req, res) => {
+// GET Request
+productsRouter.route("/").get((req, res) => {
   Products.find()
     .then((products) => {
       if (products) {
@@ -41,61 +22,38 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/name/:name").get((req, res) => {
-  console.log(req.params.name);
-  // Products.find({ name: req.params.name })
-  Products.find({ name: { $regex: "apple", $options: "i" } })
-    .then((products) => {
-      if (products) {
-        res.status(200).json({
-          products: products,
-          req: {
-            type: "GET",
-            url: "http://localhost:5000/api/v1/categories/",
-          },
-        });
-      }
-    })
-
-    .catch((err) => res.status(400).json("Error: " + err));
+//POST Request to add
+productsRouter.post("/add", (req, res) => {
+  const { name, description, productImage, date } = req.body;
+  if (!name || !description || !productImage || !date) {
+    return res
+      .status(400)
+      .json({ message: "name,description,productImage and date is required" });
+  } else {
+    const newProducts = new Products({
+      name,
+      description,
+      productImage,
+      date,
+    });
+    newProducts
+      .save()
+      .then(() => res.json("Products added!"))
+      .catch((err) => res.status(400).json("Error: " + err));
+  }
 });
 
-// const upload = multer({ storage: storage });
-//post request
-//categories/add
-router.post("/add", upload.single("productImage"), (req, res) => {
-  console.log(req.file);
-  const name = req.body.name;
-  const description = req.body.description;
-  const productImage = req.body.productImage;
-  const date = req.body.date;
+//GET using ID
 
-  const newProducts = new Products({
-    name,
-    description,
-    productImage,
-    date,
-  });
-
-  newProducts
-    .save()
-    .then(() => res.json("Products added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.route("/:id").get((req, res) => {
+productsRouter.get("/:id", (req, res) => {
   Products.findById(req.params.id)
     .then((products) => res.json(products))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json({ message: "Products Not Found" }));
 });
 
-router.route("/:id").delete((req, res) => {
-  Products.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Products deleted."))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+//POST update
 
-router.route("/update/:id").post((req, res) => {
+productsRouter.post("/update/:id", (req, res) => {
   Products.findById(req.params.id)
     .then((products) => {
       products.name = req.body.name;
@@ -108,7 +66,14 @@ router.route("/update/:id").post((req, res) => {
         .then(() => res.json("Products updated!"))
         .catch((err) => res.status(400).json("Error: " + err));
     })
-    .catch((err) => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json({ message: "Product Not Found" }));
 });
 
-module.exports = router;
+//DELETE
+productsRouter.delete("/:id", (req, res) => {
+  Products.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Products deleted."))
+    .catch((err) => res.status(400).json({ message: "Product Not Found" }));
+});
+
+module.exports = productsRouter;
